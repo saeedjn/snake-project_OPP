896309441344838fa61a10e42e10f6dfbceb249b
SaeedJN <saeedi1372@yahoo.com>
2025-09-14 13:49:39 +0330

    change in food class and generate new food method

--- food.py
+++ food.py
@@ -5,17 +5,23 @@ from color import RED
 
 
 class Food:
-    def __init__(self,cols, rows, cell_size):
+    def __init__(self,cols, rows, cell_size, snake_body=None, walls=None):
         self.cols = cols
         self.rows = rows
         self.cell_size = cell_size
         self.position = (0,0)
-        self.respawn()
+        self.snake_body = snake_body if snake_body else []
+        self.walls = walls if walls else []
+        self.generate_new_food()
 
-    def respawn(self):
-        x = random.randint(0,self.cols-1)
-        y = random.randint(0,self.rows-1)
-        self.position = (x,y)
+    def generate_new_food(self):
+        while True:
+            x = random.randint(0,self.cols-1)
+            y = random.randint(0,self.rows-1)
+            pos = (x,y)
+            if pos not in self.snake_body and pos not in self.walls:
+                self.position = pos
+                break
 
     def get_position(self):
         return self.position
